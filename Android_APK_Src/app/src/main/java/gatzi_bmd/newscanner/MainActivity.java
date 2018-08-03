2eab13d9d90eb85560269cd7b4be9829d7f64891
MrGatzi <martingatzweiler@gmail.com>
2018-08-03 17:10:30 +0200

    test commit to show Github
    totaly cool desc

--- Android_APK_Src/app/src/main/java/gatzi_bmd/newscanner/MainActivity.java
+++ Android_APK_Src/app/src/main/java/gatzi_bmd/newscanner/MainActivity.java
@@ -89,9 +89,8 @@ public class MainActivity extends Activity implements OnClickListener {
                     REQUEST_EXTERNAL_STORAGE
             );
         }
-
         super.onCreate(savedInstanceState);
-
+        //test to show github
         setContentView(R.layout.activity_main);
 
 
